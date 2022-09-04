e92d9a43a811bc4af40e9e82c4107a2de3315302
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2022-09-04 21:27:41 +1000

    🔧 Fixed base hosting on config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import * as path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "set-revision/",
+    base: "/set-revision/",
     plugins: [react()],
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
