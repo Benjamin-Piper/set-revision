9809afcd423b95de63e445060cb0da4358d2165d
Benjamin Piper <68324342+Benjamin-Piper@users.noreply.github.com>
2022-09-04 17:20:18 +1000

    🔧 Fixed GitHub pages hosting

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+    base: "set-revision/",
     plugins: [react()],
     resolve: {
         alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
